2007bee21bf211daf12e305189c0435c6d524c23
Nishant <nishraut7@gmail.com>
2023-10-04 18:54:06 +0530

    added userHeader component.

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import UserHeader from '../components/UserHeader'
 
 const UserPage = () => {
   return (
-    <div>
-      <h1>Userpage</h1>
-    </div>
+    <>
+        <UserHeader />
+    </>
   )
 }
 
