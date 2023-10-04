975a500a4214a8c016c8cd434cd6451a2053a371
Nishant <nishraut7@gmail.com>
2023-10-04 11:42:04 +0530

    Initial setup of project.

---
+++ src/main.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { mode } from "@chakra-ui/theme-tools";
+import { ColorModeScript } from '@chakra-ui/react';
+
+// following code helps to switch between light and dark theme. first color is light theme color whereas second one is dark color
+const styles = {
+  global: (props) => ({
+    body: {
+      color: mode("grey.800", "whiteAlpha.900")(props),
+      bg: mode("grey.100", "#101010")(props)
+    }
+  })
+};
+
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: true
+};
+
+const colors = {
+  gray: {
+    light: "#616161",
+    dark: "#1e1e1e"
+  }
+};
+
+const theme = extendTheme({ config, styles, colors });
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>,
+)
